ff39acd2d32efcd6bf15a7acd5c1328e68054590
Nikasyauskas <nikasyauskas@gmail.com>
2023-01-02 23:03:40 +0300

    ddl container

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from simple_ddl_parser import parse_from_file
-import pprint
+import dearpygui.dearpygui as dpg
+
+# TODO function - ddl.py -> DDLContainer -> __cast_hive_to_spark_type() not cover all type
+
 
 
 if __name__ == '__main__':
-    parse_results = parse_from_file('resources/hive_ddl_test.sql')
 
-    pp = pprint.PrettyPrinter(indent=4)
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=1200, height=1200)
 
-    pp.pprint(parse_results)
+    with dpg.window(label="Example Window"):
+        dpg.add_text("Hello, world")
+        dpg.add_button(label="Save")
+        dpg.add_input_text(label="string", default_value="Quick brown fox")
+        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
 
--- resources/hive_ddl_test.sql
+++ resources/hive_ddl_test.sql
@@ -1,10 +1,8 @@
-create table `custom_rozn_producshelf.ft_test` (
-	`epk_id` bigint,
-	`channel_id` int,
-	row_update_time timestamp,
-	`row_actual_from` string
-)
-partitioned by (`row_actual_to` string)
-ROW FORMAT SERDE 'parquet.hive.serde.ParquetHiveSerDe'
-STORED AS INPUTFORMAT 'parquet.hive.DeprecatedParquetInputFormat'
-OUTPUTFORMAT 'parquet.hive.DeprecatedParquetOutputFormat';
+CREATE TABLE IF NOT EXISTS test_schema.test_table (
+ epk_id int,
+ name string,
+ row_update_time timestamp,
+ row_actual_from date )
+PARTITIONED BY (row_actual_to string)
+STORED AS PARQUET
+;
